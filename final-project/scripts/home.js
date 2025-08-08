b125afa943d5e92a7c926c62e3f34d83e03adb40
hlquantz-byui <hquantz@byupathway.edu>
2025-08-08 07:40:02 -0600

    Refactor code formatting in home.js and main.js for improved readability

--- final-project/scripts/home.js
+++ final-project/scripts/home.js
@@ -1,14 +1,1 @@
-import { fetchRecipes } from './data.js';
-import { getRandomItems } from './utils.js';
-import { displayRecipes } from './recipe.js';
-
-export async function loadHomepageRecipes() {
-    const allRecipes = await fetchRecipes();
-
-    const featured = getRandomItems(allRecipes, 3);
-
-    const container = document.querySelector('#recipe-container');
-
-    displayRecipes(featured, container);
-}
 import { fetchRecipes } from "./data.js"; import { getRandomItems } from "./utils.js"; import { displayRecipes } from "./recipe.js"; async function loadHomepageRecipes() { var e = await fetchRecipes(), e = getRandomItems(e, 3), i = document.querySelector("#recipe-container"); displayRecipes(e, i) } export { loadHomepageRecipes };
